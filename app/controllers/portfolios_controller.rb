95e804d4ad520a8f8206ac0102177b0a1acaf7bc
florian.marchal <florian.marchal@coupa.com>
2018-10-09 11:40:50 +0200

    Integrated new and create actions for Portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,22 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      @portfolio_item.save ?
+          format.html {redirect_to portfolios_path, notice: 'Portfolio was created.'} :
+          format.html {render :new}
+    end
+  end
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
 end

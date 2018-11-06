535f32c2dd6a2bc26492acd78fbd6a5957ffbf27
florian.marchal <florian.marchal@coupa.com>
2018-11-06 17:06:29 +0100

    Built out initial form and partial call for comments along with controller actions

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_blog, only: [:edit, :update, :destroy, :toggle_status]
   layout "blog"
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
@@ -13,6 +13,9 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+    @blog = Blog.includes(:comments).friendly.find(params[:id])
+    @comment = Comment.new
+
     @page_title = @blog.title
     @seo_keywords = @blog.body
   end
@@ -75,7 +78,7 @@ class BlogsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_blog
-    @blog = Blog.friendly.find(params[:id])
+    @blog = Blog.includes(:comments).friendly.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
